519e55d89f21ad54ea1b9e71752d266147b63c0b
badal <badal.bahinipati99@gmail.com>
2022-05-31 11:56:47 +0530

    drive download commit 2

--- app/src/main/java/com/example/demodownloader/DriveDownload.java
+++ app/src/main/java/com/example/demodownloader/DriveDownload.java
@@ -2,7 +2,14 @@ package com.example.demodownloader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DownloadManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.CookieManager;
+import android.webkit.URLUtil;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DriveDownload extends AppCompatActivity {
 
@@ -10,5 +17,23 @@ public class DriveDownload extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drive_download);
+
+        EditText urL = (EditText) findViewById(R.id.url);
+        Button download = (Button) findViewById(R.id.download);
+
+        download.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getUrl = urL.getText().toString();
+                DownloadManager.Request req = new DownloadManager.Request(Uri.parse(getUrl));
+                String title = URLUtil.guessFileName(getUrl, null, null);
+                req.setTitle(title);
+                req.setDescription("Downloading files...");
+                String cookie = CookieManager.getInstance().getCookie(getUrl);
+                req.addRequestHeader("cookie", cookie);
+                req.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                
+            }
+        });
     }
 }
