03e57ccfedd42db224e1022b2daa6b17ec07cc91
badal <badal.bahinipati99@gmail.com>
2022-05-31 14:04:45 +0530

    Final commit

--- app/src/main/java/com/example/demodownloader/DownloadOptions.java
+++ app/src/main/java/com/example/demodownloader/DownloadOptions.java
@@ -1,8 +1,13 @@
 package com.example.demodownloader;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +31,39 @@ public class DownloadOptions extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        dev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkPermission()){
+                    //allowed
+                    Intent in = new Intent(DownloadOptions.this, FileManager.class);
+                    startActivity(in);
+                }
+                else{
+                    //request permission
+                    requestPermission();
+                }
+            }
+        });
+    }
+
+    private void requestPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(DownloadOptions.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+           Toast.makeText(DownloadOptions.this, "Allow storage access", Toast.LENGTH_SHORT).show();
+        }
+        else
+          ActivityCompat.requestPermissions(DownloadOptions.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 111);
+        }
+
+    private boolean checkPermission(){
+        int val = ContextCompat.checkSelfPermission(DownloadOptions.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(val == PackageManager.PERMISSION_GRANTED)
+            return true;
+        else
+            return false;
+
+
 
     }
 }
--- app/src/main/java/com/example/demodownloader/WriteFile.java
+++ app/src/main/java/com/example/demodownloader/WriteFile.java
@@ -7,29 +7,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class WriteFile extends AppCompatActivity {
     Button write;
+    Button read;
     EditText txt;
     String filename;
+    TextView fileContent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_file);
 
         write = (Button) findViewById(R.id.write);
+        read = (Button) findViewById(R.id.read);
         txt = (EditText) findViewById(R.id.text);
+        fileContent = (TextView) findViewById(R.id.content);
         filename = "TestFile.txt";
 
         write.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 write(v);
+            }
+        });
+
+        read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                read(v);
             }
         });
     }
@@ -46,5 +59,21 @@ public class WriteFile extends AppCompatActivity {
         }
         txt.setText("This is test file...");
         Toast.makeText(WriteFile.this, "Creating file...", Toast.LENGTH_SHORT).show();
+    }
+
+    public void read(View v){
+        try{
+            FileInputStream fis = openFileInput(filename);
+            int x;
+            StringBuilder sb = new StringBuilder();
+            while((x = fis.read()) != -1){
+                sb.append((char)x);
+            }
+            fileContent.setText((sb.toString()));
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        Toast.makeText(WriteFile.this, "Loading your file...", Toast.LENGTH_SHORT).show();
     }
 }
